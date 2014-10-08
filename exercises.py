b4c9a149b659721904964a1fd79e517e15077140
patrickh <jpatrickhall@gmail.com>
2014-10-07 22:25:31 -0400

    proof read ex. 2 and 3

--- exercises.py
+++ exercises.py
@@ -9,7 +9,7 @@ Python exercises for Bellarmine Analytics program
 1.) Working With Strings
 2.) Loops and File I/O 
 3.) Lists, Dictionarys and Sets
-4.) Scrping Data From the Web 
+4.) Scraping Data From the Web 
 5.) Numpy and Scipy 
 6.) Kaggle Titanic Competition 
 7.) Graphing Results
@@ -56,3 +56,80 @@ user_string3= 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, lik
 ### EXERCISE 1.6: Use a simple string method to decide whether users 2 and 3 use the same operating system as user 1.
 
 ### EXERCISE 1.7: Print a tab-separated table of each user's operating system and broswer.
+
+### EXERCISE 2: LOOPS AND FILE I/O ############################################ 
+### In exercises 2 and 3 we will clean up several example dating profiles. Once we have 
+### the text ready for analysis, we will try to make the best match between profiles in exercise 3.
+
+# REFERENCES:
+# https://docs.python.org/2/tutorial/controlflow.html
+# https://docs.python.org/2/tutorial/inputoutput.html#reading-and-writing-files
+# https://docs.python.org/2/library/stdtypes.html#string-methods
+
+### EXERCISE 2.1: Count the number of lines in the profiles_raw.txt file using a for loop.
+
+### EXERCISE 2.2: Remove all non-alphabetic characters from the profiles_raw.txt file.
+### Save the new file as profiles_clean.txt.                   
+
+### EXERCISE 2.3: Modify your code from exercise 2.2 (remove all non-alphabetic 
+# characters from the profiles_raw.txt) AND to convert all lines of new the 
+# 'profiles_clean.txt' file to lowercase.   
+  
+### EXERCISE 2.4: Use the solutions of exercises 2.1 (counting the number of 
+# profiles_raw.txt) and 2.3 (removing all non-alphabetic character and converting 
+# to lowercase) to add a progress indicator to our file cleaning code. The 
+# progress indicator should tell you which line of the file your program is 
+# working on.   
+  
+### Why do you think progress indicator's are important? 
+        
+### EXERCISE 3: LISTS, DICTIONARIES, AND SETS ################################# 
+### In exercises 2 and 3 we will clean up several example dating profiles. Once we have 
+### the text ready for analysis, we will try to make the best match between profiles in exercise 3.
+
+# REFERENCES:
+# https://docs.python.org/2/tutorial/datastructures.html
+# https://docs.python.org/2/library/collections.html
+
+### EXERCISE 3.1: Turn each line into a list to count the number of words in 
+# the profiles_raw.txt file.
+
+### EXERCISE 3.2: Use list comprehensions to re-write the solution to exercise 2.4
+# (remove all non-alphabetic characters from the profiles_raw.txt and convert
+# to lowercase) in a more pythonic fashion.
+
+### Exercise 3.3: Modify the list comprehension in exercise 3.2 (remove all 
+# non-alphabetic characters from the profiles_raw.txt and convert to lowercase) 
+# to also remove words with 3 or less characters in addition to the other cleaning tasks. 
+
+### Exercise 3.4: Use collections, lists, sets, and dictionaries to create a set of frequently (10+)
+# occuring terms in the cleaned profiles and keep only them in the profiles. Create 
+# a new file called 'profiles_cleaned_freq.txt'.
+
+# (HINT) Import the collections module 
+
+# (HINT) Create a list of every word in the cleaned profiles
+
+# (HINT) Create a dictionary of term counts using the Counter collection               
+
+# (HINT) Create a set that keeps only terms in the dictionary that occured more that ten times        
+
+# (HINT) Compare a list of the words in a line to the set of frequent terms to 
+# keep only the frequently occuring terms 
+     
+### Exercise 3.5: Use collections and dictionaries to create a dictionary of term counts 
+# for each profile. Use these dictionaries of term frequencies to decide which 
+# of the profiles are the best match. Print the term counts to a new file called 
+# 'profiles_term_counts'
+
+# (HINT) Import collections module
+
+# (HINT) Print a dictionary of term counts for each individual cleaned profile
+
+# Which of these profiles looks the most compatible to you? 
+# (HINT) Compare the term counts of important words.           
+
+
+
+
+
--- solutions.py
+++ solutions.py
@@ -9,7 +9,7 @@ Python exercise SOLUTIONS for Bellarmine Analytics program
 1.) Working With Strings
 2.) Loops and File I/O 
 3.) Lists, Dictionarys and Sets
-4.) Scrping Data From the Web 
+4.) Scraping Data From the Web 
 5.) Numpy and Scipy 
 6.) Kaggle Titanic Competition 
 7.) Graphing Results
@@ -20,8 +20,11 @@ Python exercise SOLUTIONS for Bellarmine Analytics program
 
 ### This string represents a user being logged by a web server
 ### It contains information such as the user's operating system and browser
+
+#%%
 user_string1= 'Mozilla/5.0 (Windows NT 6.1; WOW64) App3leWebKit/53.1 (KHTML, like Gecko) Version/4.0 Safari/533.1'
 print user_string1
+#%%
 
 # SLICING
 # REFERENCE: https://docs.python.org/2/tutorial/introduction.html#strings
@@ -32,21 +35,25 @@ print user_string1
 # 0   1   2   3   4   5   6
 #-6  -5  -4  -3  -2  -1
 
+#%%
 ### EXERCISE 1.1: Print only the user's operating system. 
 print user_string1[13:27]
 
+#%%
 ### EXERCISE 1.2: Print only the user's browser.  
 print user_string1[-12:-6]
 
+#%%
 # ESCAPE CHARACTERS 
 # REFERENCE: https://docs.python.org/2/tutorial/introduction.html#strings
 
 ### EXERCISE 1.3: Print the user's operating system and browser as a single tab-delimited string
 print user_string1[13:27] + '\t' + user_string1[-12:-6]
-
+#%%
 ### EXERCISE 1.4: Print the user's operating system and browser on separate lines using only one python print statement. 
 print user_string1[13:27] + '\n'+ user_string1[-12:-6]
 
+#%%
 ### EXERCISE 1.5: What are some other common escape characters?
 
 """
@@ -61,11 +68,13 @@ Other common escape characters include:
 
 """
 
+#%%
 # STRING FUNCTIONS
 # REFERENCE: https://docs.python.org/2/library/stdtypes.html#string-methods
 
 user_string2= 'Mozilla/5.0 (Linux; Android 2.2) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Mobile Safari/536.5'
 user_string3= 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5'
+#%%
 
 ### EXERCISE 1.5: Convert user_string2 and user_string3 to lowercase and print them. Why is this a good idea?
 
@@ -77,17 +86,19 @@ print lower3
 """ 
 
 This is a good idea because now you can search these strings for terms without
-worry about the many different possible cases of the strings you would like to 
+worrying about the many different possible cases of the strings you would like to 
 find.
 
 """ 
+#%%
 
 ### EXERCISE 1.6: Use a simple string method to decide whether users 2 and 3 use the same operating system as user 1.
 
-user1_OS= user_string1[13:27].lower()
-print lower2.find(user1_OS) > 0
-print lower3.find(user1_OS) > 0
+user1_OS= user_string1[13:27].lower() # OS for User 1
+print lower2.find(user1_OS) > 0 # Test if User 1 OS in found in User 2 log record 
+print lower3.find(user1_OS) > 0 # Test if User 1 OS in found in User 3 log record 
 
+#%%
 ### EXERCISE 1.7: Print a tab-separated table of each user's operating system and broswer.
 
 line1= 'user1\t' + user_string1[13:27] + '\t' + user_string1[-12:-6]
@@ -95,6 +106,7 @@ line2= 'user2\t' + user_string2[13:27] + '\t' + user_string2[-12:-6]
 line3= 'user3\t' + user_string3[13:27] + '\t' + user_string3[-12:-6]
 print '\n'.join((line1, line2, line3))
 
+#%%
 ### EXERCISE 2: LOOPS AND FILE I/O ############################################ 
 ### In exercises 2 and 3 we will clean up several example dating profiles. Once we have 
 ### the text ready for analysis, we will try to make the best match between profiles in exercise 3.
@@ -111,9 +123,9 @@ with open('profiles_raw.txt') as f:
         pass
 print 'The number of lines is:  %i.' % (i+1)
 
+#%%
 ### EXERCISE 2.2: Remove all non-alphabetic characters from the profiles_raw.txt file.
 ### Save the new file as profiles_clean.txt.                   
-
 clean_line= ''
 with open('profiles_raw.txt') as in_file:
     with open('profiles_clean.txt', 'w') as out_file:
@@ -123,10 +135,10 @@ with open('profiles_raw.txt') as in_file:
                     clean_line+= character
             out_file.write(clean_line)
 
+#%%
 ### EXERCISE 2.3: Modify your code from exercise 2.2 to remove all non-alphabetic 
 # characters from the profiles_raw.txt file and to convert all lines of new the 
 # 'profiles_clean.txt' file to lowercase.   
-
 clean_line= ''
 with open('profiles_raw.txt') as in_file:
     with open('profiles_clean.txt', 'w') as out_file:
@@ -136,10 +148,10 @@ with open('profiles_raw.txt') as in_file:
                     clean_line+= character
                 clean_line= clean_line.lower()
             out_file.write(clean_line)
-            
+  
+#%%          
 ### EXERCISE 2.4: Use the solutions of exercises 2.1 and 2.3 to create a progress indicator 
-# for our file cleaning code.   
-            
+# for our file cleaning code.               
 i= 0
 with open('profiles_raw.txt') as f:
     for i, line in enumerate(f): 
@@ -157,7 +169,26 @@ with open('profiles_raw.txt') as in_file:
             out_file.write(clean_line)
             print 'Line %i/%i cleaned ...' % (j+1, i+1)
 print 'Done.'
-         
+
+### Why are progress indicator's important? 
+
+""" 
+
+Progress indicators are important because sometimes jobs take a long time, or 
+sometimes you might be unsure how long a job will take. Progress indicators 
+help prevent:
+
+1.) Starting a job, waiting for hours, and finding out it has only 
+done a small portion of it's work (because you underestimated something).
+
+OR 
+
+2.) After waiting for a job to finish for a long time you might get frustrated 
+and stop it ... just to find out it was almost finished !!!
+
+"""
+
+#%%         
 ### EXERCISE 3: LISTS, DICTIONARIES, AND SETS ################################# 
 ### In exercises 2 and 3 we will clean up several example dating profiles. Once we have 
 ### the text ready for analysis, we will try to make the best match between profiles in exercise 3.
@@ -174,9 +205,9 @@ with open('profiles_raw.txt') as f:
         i+= len(line.split())
 print 'The number of words is:  %i.' % (i)
 
+#%%
 ### EXERCISE 3.2: Use list comprehensions to re-write the solution to exercise 2.4
 # in a more pythonic fashion.
-
 nlines= sum(1 for line in open('profiles_raw.txt'))
 print 'The number of lines is:  %i.' % (nlines)
 
@@ -187,6 +218,7 @@ with open('profiles_raw.txt') as in_file:
             print 'Line %i/%i cleaned ...' % (j+1, nlines)
 print 'Done.'
 
+#%%
 ### Exercise 3.3: Modify the list comprehension in exercise 3.2 to remove words 
 # with 3 or less characters in addition to the other cleaning tasks. 
 
@@ -200,11 +232,10 @@ with open('profiles_raw.txt') as in_file:
             print 'Line %i/%i cleaned ...' % (j+1, nlines)
 print 'Done.'
 
-### Exercise 3.4: Use collections, lists, sets, and dictionaries to create a set of infrequently 
-# occuring terms in the cleaned profiles and remove them from the profiles. Create 
-# a new file called 'profiles_cleaned_freq.txt'.
-
 #%%
+### Exercise 3.4: Use collections, lists, sets, and dictionaries to create a set of frequently (10+)
+# occuring terms in the cleaned profiles and keep only them in the profiles. Create 
+# a new file called 'profiles_cleaned_freq.txt'.
 
 # Import the collections module 
 import collections
@@ -229,13 +260,12 @@ with open('profiles_clean.txt') as in_file:
             line_list= line.split()
             freq_list= [word for word in line_list if word in keep_set]
             out_file.write(' '.join(freq_list) + '\n')
-            
+
+#%%            
 ### Exercise 3.5: Use collections and dictionaries to create a dictionary of term counts 
 # for each profile. Use these dictionaries of term frequencies to decide which 
 # of the profiles are the best match. Print the term counts to a new file called 
-# 'profiles_term_counts'
-
-#%%
+# 'profiles_term_counts.txt'.
 
 # Import collections module
 import collections
@@ -251,7 +281,6 @@ with open('profiles_clean_freq.txt') as in_file:
              for key in term_counts:
                  out_file.write(key + ' ' + str(term_counts[key]) + '\n')
              
-
 #%%
 
 
