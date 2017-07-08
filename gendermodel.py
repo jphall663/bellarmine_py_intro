19fafaccff53c5f53e41564168af52761c86b396
p h <jpatrickhall@gmail.com>
2017-07-08 13:58:36 -0400

    more updates

--- gendermodel.py
+++ gendermodel.py
@@ -1,20 +1,5 @@
 """
 
-Copyright (c) 2015 by Patrick Hall, jpatrickhall@gmail.com
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-   
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
--------------------------------------------------------------------------------
-
 This simple code is desinged to teach a basic user to read in the files in
 python, simply find what proportion of males and females survived and make
 a predictive model based on this.
@@ -110,4 +95,3 @@ for row in test_file_object:
         predictions_file_object.writerow([row[0], "0"])
 test_file.close()
 predictions_file.close()
-
